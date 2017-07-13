092683cccef2b4a193b812e9c3bfd524d3bd4e8f
skaixis <648120201@qq.com>
2017-07-14 03:00:57 +0800

    change decode

---
+++ DecodeTest.py
@@ -0,0 +1,14 @@
+def NewEncode(value,index):
+    t=(value<<5)+index
+    print(t)
+    return [int(t/100)+1,(t%100)+101]
+def NewDecode(t):
+    t=100*(t[0]-1)+t[1]-101
+    index=t&15
+    value=t>>5
+    return [value,index]
+while 1:
+    value=int(input())
+    index=int(input())
+    t=NewEncode(value,index)
+    print(NewDecode(t))
--- sketch_may22a/sketch_may22a.ino
+++ sketch_may22a/sketch_may22a.ino
@@ -1,6 +1,6 @@
-const int sizes=30;
+const int sizes=64;
 const int Ports[]={0,2,3,4,5,6,7};
-int Re[sizes];
+int Re[sizes]={0};
 int n;
 void setup() {
   // put your setup code here, to run once:
@@ -21,14 +21,8 @@ while(Serial.available()){//读取串口缓冲区所有数据
   //Serial.println(Re[n]);
   n++;
     }
-if(Re[n-1]!=100){//当最后一位不是规定的分隔位100，继续读取至空
-  while(Serial.available()){
-  Re[n]=Serial.read();
-  //Serial.println(Re[n]+1000);
-  n++;
-  }
-  }
-if((Re[n-1]!=100)&&(Re[n-1]!=0)){
+
+if((Re[n-1]<=100){
   Serial.println("data wrong!");//最后一位仍然不是100，发送数据错误信息
   for(int i=0;i<sizes;i++){//数据错误清空数据
   Re[i]=0;
@@ -38,34 +32,28 @@ if((Re[n-1]!=100)&&(Re[n-1]!=0)){
 n=0;
 while(Re[n]!=0){//解码部分
   Serial.print("d");
-  if(Is099(Re[n])&&Is099(Re[n+1])&&(Re[n+2]==100)){//第一个和第二个数字是0-99，第三位是分隔号100
-    int t=Re[n]*100+Re[n+1];
+  if(Is1100(Re[n])&&(Re[n+1]>100)){//第一个和第二个数字是0-99，第三位是分隔号100
+    int t=(Re[n]-1)*100+Re[n+1]-101;
     Decode(t);}
   else {
     n++;
-    if(Is099(Re[n])&&Is099(Re[n+1])&&(Re[n+2]==100)){//第一个和第二个数字是0-99，第三位是分隔号100
+    if(Is1100(Re[n])&&(Re[n+1]>100)){//第一个和第二个数字是0-99，第三位是分隔号100
     int t=Re[n]*100+Re[n+1];
     Decode(t);}  
-    else {
-      n++;
-    if(Is099(Re[n])&&Is099(Re[n+1])&&(Re[n+2]==100)){//第一个和第二个数字是0-99，第三位是分隔号100
-    int t=Re[n]*100+Re[n+1];
-    Decode(t);}  
-      }
   }
-n+=3;
+n+=2;
   }
 for(int i=0;i<sizes;i++){//处理完数据重置数组
   Re[i]=0;
   }
 }
-bool Is099(int t){
-  if(t>=0&&t<100)return true;
+bool Is1100(int t){
+  if(t>=1&&t<101)return true;
   else return false;
   }
 void Decode(int code){
-int Port=code&7;
-int Value=code>>4;
+int Port=code&15;
+int Value=code>>5;
   Action(Port,Value);
   }
 void Action(int Port,int Value){
